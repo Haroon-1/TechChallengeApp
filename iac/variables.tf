388550c854782d64cb4c5011e1311c289f8cda77
Haroon Dogar <haroondogar93@gmail.com>
2021-03-11 01:38:47 +0000

    feat: terraform config resource provisioner

---
+++ iac/variables.tf
@@ -0,0 +1,27 @@
+## Azure config variables ##
+variable location {
+  default = "australiaeast"
+}
+
+## Resource group variables ##
+variable resource_group_name {
+  default = "serviantechchallenge"
+}
+
+
+## AKS kubernetes cluster variables ##
+variable cluster_name {
+  default = "k8-cluster-1"
+}
+
+variable "agent_count" {
+  default = 2
+}
+
+variable "dns_prefix" {
+  default = "k8"
+}
+
+variable "admin_username" {
+    default = "hdogar"
+}
