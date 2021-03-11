f340e62c7fdc7c8c84abcf3269fc4547e2b598c3
Haroon-1 <haroondogar93@gmail.com>
2021-03-11 18:23:38 +1100

    fix: admin_enabled removed

--- iac/main.tf
+++ iac/main.tf
@@ -65,7 +65,6 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name      = azurerm_resource_group.techchallenge_k8.name
   location                 = azurerm_resource_group.techchallenge_k8.location
   sku                      = "Basic"
-  admin_enabled            = false
 }
 
 ## Outputs ##
